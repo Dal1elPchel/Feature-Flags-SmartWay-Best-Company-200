f7750d6e4ef91bdec74f0891288b3aa5b0284737
Daniel <danielpchelincev123@gmail.com>
2026-06-26 18:34:50 +0300

    изменены компоненты под форму авторизации

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -4,17 +4,20 @@ import {ReactNode} from "react";
 interface ButtonProps {
     text: string | ReactNode;
     isAccent: boolean;
-    onClick: () => void;
+    onClick?: () => void;
+    isSubmit: boolean;
+    className?: string;
 }
 
 const Button =
-    ({text, isAccent, onClick}: ButtonProps) =>
+    ({text, isAccent, onClick, isSubmit, className}: ButtonProps) =>
     {
         const styleList = [styles.Button];
         if (isAccent) styleList.push(styles.accent);
+        if (className) styleList.push(className);
 
         return (
-            <button className={styleList.join(" ")}
+            <button type={ isSubmit ? "submit" : "button"} className={styleList.join(" ")}
             onClick={onClick}>{text}</button>
         );
 }
--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,28 +1,24 @@
 import styles from "./Input.module.scss";
+import {InputHTMLAttributes, forwardRef} from "react";
 
 type InputType = "text" | "password";
 
-interface InputProps {
-    name: string;
-    placeHolder: string;
+interface InputProps extends InputHTMLAttributes<HTMLInputElement>{
     typeInput: InputType;
-    value: string;
-    onChange: (value: string) => void;
-    isRequired: boolean;
 }
 
-const Input = ({name, placeHolder, typeInput, value = "",
-               onChange = () => {}, isRequired = true}: InputProps) => {
+const Input =
+    forwardRef<HTMLInputElement, InputProps>(
+        ({typeInput, ...rest}, ref) => {
     return (
         <input className={styles.inputElem}
-        name={name}
-        id={name}
-        placeholder={placeHolder}
+        ref={ref}
         type={typeInput}
-        value={value}
-        required={isRequired}
-        onChange={(e) => onChange(e.target.value)}/>
+        {...rest}
+        />
     );
-}
+});
+
+Input.displayName = "Input";
 
 export default Input;
