fc29eea738b50dbb80164254273f28663d01a25e
Daniel <danielpchelincev123@gmail.com>
2026-07-01 17:56:16 +0300

    добавил компонент информации для пользователя из категории загружки данных и ошибок

---
+++ src/components/InfoMessage/InfoMessage.tsx
@@ -0,0 +1,26 @@
+import styles from "./InfoMessage.module.scss";
+import {X} from "lucide-react";
+
+
+interface ErrorProps {
+    message: string | null;
+    status: "success" | "loading" | "error";
+    onClose?: () => void;
+}
+
+const InfoMessage = ({message, status, onClose}: ErrorProps) => {
+    var styleOfMessage = styles.infoMessage;
+    if (status === "success") styleOfMessage = styles.successMessage;
+    if (status === "error") styleOfMessage = styles.errorMessage;
+
+    return (
+        <div className={`${styles.infoMessage} ${styleOfMessage}`}>
+            {message ? message : "Неизвестное сообщение"}
+            {status === "error" && (
+                <button onClick={onClose}><X/></button>
+            )}
+        </div>
+    );
+}
+
+export default InfoMessage;
