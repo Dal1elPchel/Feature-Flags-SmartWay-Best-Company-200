212f95b5c655c819b62375cae3b9e0dbf3bb5859
Daniel <danielpchelincev123@gmail.com>
2026-07-03 12:03:26 +0300

    сделал более стабильную загрузку профиля

--- src/Routes/PrivateRoute.tsx
+++ src/Routes/PrivateRoute.tsx
@@ -2,8 +2,13 @@ import userStore from "../stores/UserStore.ts";
 import {observer} from "mobx-react-lite";
 import {Navigate} from "react-router-dom";
 import {Outlet} from "react-router-dom";
+import InfoMessage from "../components/InfoMessage/InfoMessage.tsx";
 
 const PrivateRoute = observer(() => {
+
+    if (!userStore.isInitialized) {
+        return <InfoMessage message={"Загрузка профиля..."} status={"loading"}/>
+    }
 
     if (!userStore.isAuth) {
         return <Navigate to="/login" replace />;
