e237bad1723f32eb2cf8c7c9c7b66438628e1f6e
Daniel <danielpchelincev123@gmail.com>
2026-07-03 21:37:46 +0300

    страница логика под другим именем

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import { Routes, Route } from 'react-router-dom';
-import LoginPage from './pages/LoginPage/loginPage.tsx';
+import {Routes, Route} from 'react-router-dom';
+import LoginPage from './pages/LoginPage/LoginPage.tsx';
 import PrivateRoute from './Routes/PrivateRoute.tsx';
 import MainLayout from './layouts/MainLayout/MainLayout.tsx';
 import AddPage from './pages/AddPage/AddPage.tsx';
@@ -11,13 +11,13 @@ function App() {
     return (
         <>
             <Routes>
-                <Route path={'/login'} element={<LoginPage />} />
-                <Route element={<PrivateRoute />}>
-                    <Route element={<MainLayout />}>
-                        <Route path={'/'} element={<TablePage />} />
-                        <Route path={'/add'} element={<AddPage />} />
-                        <Route path={'/flags/:id'} element={<FlagCardPage />} />
-                        <Route path={'/flags/:id/edit'} element={<EditPage />} />
+                <Route path={'/login'} element={<LoginPage/>}/>
+                <Route element={<PrivateRoute/>}>
+                    <Route element={<MainLayout/>}>
+                        <Route path={'/'} element={<TablePage/>}/>
+                        <Route path={'/add'} element={<AddPage/>}/>
+                        <Route path={'/flags/:id'} element={<FlagCardPage/>}/>
+                        <Route path={'/flags/:id/edit'} element={<EditPage/>}/>
                     </Route>
                 </Route>
             </Routes>
